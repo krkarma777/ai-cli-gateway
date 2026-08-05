4ca5d7803b51056f4e3f5f67de5d6dca50647d90
krkarma777 <krkarma777@gmail.com>
2026-08-05 12:08:44 +0900

    test: add ready Codex CLI fixture

---
+++ internal/testcli/cmd/fake-codex-cli/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"os"
+
+	"github.com/krkarma777/ai-cli-gateway/internal/testcli"
+)
+
+func main() {
+	os.Exit(testcli.CodexReadyMain(os.Args[1:], os.Stdin, os.Stdout, os.Stderr))
+}
